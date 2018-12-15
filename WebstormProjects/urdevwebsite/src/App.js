87d48e2f194f64c9f7dd017f537dda94a1236456
walker frederick <walkerfrederick@outlook.com>
2018-12-15 05:59:23 -0500

    added navbar, footer, and the main content

--- WebstormProjects/urdevwebsite/src/App.js
+++ WebstormProjects/urdevwebsite/src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 import video from "./work.mov";
 
+
+
 class App extends Component {
   render() {
     return (
         <div className="app">
-                <video autoPlay muted loop id="myVideo">
+                <video autoPlay muted loop >
                     <source src={video} type="video/mp4"/>
                 </video>
             <Navbar>
@@ -27,16 +29,36 @@ class Navbar extends Component {
     render() {
         return (
             <div className="navbar">
-
+                <a href='#' id="logo">urdev</a>
+                <div className="links">
+                    <button>ABOUT</button>
+                    <button>BLOG</button>
+                    <button>CONTACT</button>
+                </div>
             </div>
         );
     }
 }
 
 class Content extends Component {
+    setContentHeight = () => {
+        return {minHeight: window.innerHeight,}
+    }
     render() {
         return (
             <div className="content">
+                <div className="content-box" style={this.setContentHeight()}>
+                    <div className="content-left">
+                        <h1>Developers As A Service</h1>
+                        <h2>Access hundreds of developers from around the world</h2>
+                        <button>find ur dev</button>
+                    </div>
+                    <div className="content-right">
+                        <h1>Developers As A Service</h1>
+                        <h2>Access hundreds of developers from around the world</h2>
+                        <button>find ur dev</button>
+                    </div>
+                </div>
             </div>
         );
     }
