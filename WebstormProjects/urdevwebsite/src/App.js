77ac2cb8cac53e972a00eff0313a362b312a5df1
walker frederick <walkerfrederick@outlook.com>
2018-12-16 21:18:37 -0500

    bought the rights to my bg video

--- WebstormProjects/urdevwebsite/src/App.js
+++ WebstormProjects/urdevwebsite/src/App.js
@@ -1,24 +1,63 @@
 import React, { Component } from 'react';
 import './App.css';
 import video from "./work.mov";
-import iphone from "./iphone.png";
+import menu from "./menu.svg";
 
 
 
 
 class App extends Component {
+    constructor(props,){
+        super(props);
+        this.state = {
+            contentTitle: 'Development Made Simple.',
+            contentParagraph: 'The first six weeks take place entirely online, M-F 9-5 Pacific Time, using our award-winning remote delivery format. The sixth week is asynchronous. Then the program feeds into the software engineering immersive at any of our locations',
+            fadeIn: 'fade-in'
+        }
+
+    }
+
+    updateToHome = () => {
+        this.setState({
+            contentTitle: 'Development Made Simple.',
+            contentParagraph: '--'
+        })
+    }
+    updateToAbout = () => {
+        this.setState({
+            contentTitle: 'About Us',
+            contentParagraph: '--',
+        })
+    }
+    updateToContact = () => {
+        this.setState({
+            contentTitle: 'Contact Us',
+            contentParagraph: '--',
+        })
+    }
+
+
+    componentDidUpdate() {
+        this.setState();
+    }
+
   render() {
     return (
         <div className="app">
                 <video autoPlay muted loop >
                     <source src={video} type="video/mp4"/>
                 </video>
-            <Navbar>
+            <Navbar homeChange={this.updateToHome.bind(this)} aboutChange={this.updateToAbout.bind(this)} contactChange={this.updateToContact.bind(this)}>
 
             </Navbar>
-            <Content>
+            <div className="content">
+                <div className={'content-component ' + this.state.fadeIn}>
+                    <h1>{this.state.contentTitle}</h1>
+                    <p>{this.state.contentParagraph}</p>
+                    <button>Give us a call.</button>
+                </div>
 
-            </Content>
+            </div>
             <Footer>
 
             </Footer>
@@ -28,11 +67,30 @@ class App extends Component {
 }
 
 class Navbar extends Component {
+    constructor(props){
+        super(props);
+    }
     render() {
         return (
             <div className="navbar">
                 <a href='/' id="logo">urdev.xyz</a>
                 <div className="links">
+                    <button className='icon-link'>
+
+                        <svg className='icon' xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 24 24" enable-background="new 0 0 24 24" width="512px" height="512px">
+                            <g>
+                                <path d="M24,3c0-0.6-0.4-1-1-1H1C0.4,2,0,2.4,0,3v2c0,0.6,0.4,1,1,1h22c0.6,0,1-0.4,1-1V3z" fill="#69f0ae"/>
+                                <path d="M24,11c0-0.6-0.4-1-1-1H1c-0.6,0-1,0.4-1,1v2c0,0.6,0.4,1,1,1h22c0.6,0,1-0.4,1-1V11z" fill="#69f0ae"/>
+                                <path d="M24,19c0-0.6-0.4-1-1-1H1c-0.6,0-1,0.4-1,1v2c0,0.6,0.4,1,1,1h22c0.6,0,1-0.4,1-1V19z" fill="#69f0ae"/>
+                            </g>
+                        </svg>
+
+
+                    </button>
+                    <button className='link' onClick={this.props.contactChange}>CONTACT</button>
+                    <button className='link' onClick={this.props.aboutChange}>ABOUT</button>
+                    <button className='link' onClick={this.props.homeChange}>HOME</button>
+
 
                 </div>
             </div>
@@ -40,29 +98,28 @@ class Navbar extends Component {
     }
 }
 
-class Content extends Component {
-    setContentHeight = () => {
-        return {minHeight: window.innerHeight,}
+class Content extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: this.props.contentTitle,
+            paragragh: this.props.contentParagragh
+        }
     }
+
+    componentWillReceiveProps({contentTitle}) {
+        this.setState({title: this.props.contentTitle})
+    }
+
     render() {
         return (
             <div className="content">
-                <div className="content-box" style={this.setContentHeight()}>
-                    <div className="content-left fade-in">
-                        <div>
-                        <h1>Development Made Simple.</h1>
-                        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur</p>
-                            <button>view on github</button>
-                        </div>
-                    </div>
-                    <div className="content-right">
-                        <div>
-                            <h1>Development Made Simple.</h1>
-                            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur</p>
-                            <button>find ur dev</button>
-                        </div>
-                    </div>
+                <div className="content-component fade-in">
+                    <h1>{this.state.title}</h1>
+                    <p>{this.state.paragragh}</p>
+                    <button>Give us a call.</button>
                 </div>
+
             </div>
         );
     }
@@ -71,7 +128,7 @@ class Footer extends Component {
     render() {
         return (
             <div className="footer">
-
+                <span>copyright 2018</span>
             </div>
         );
     }
